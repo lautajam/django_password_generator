7edb5447258a43bd22a0d03dd875451d0b8ab7cb
Lautaro <lautajamdev@gmail.com>
2023-07-06 02:23:24 -0300

    Link the two project pages
    Now have two pages, index and about, and have a navigation bar to move from one to the other

--- django_password_generator/urls.py
+++ django_password_generator/urls.py
@@ -20,5 +20,6 @@ from generator import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path("about/", views.about, name="about")
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
--- generator/views.py
+++ generator/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def about(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'index.html')
