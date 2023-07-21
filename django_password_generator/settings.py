9a6c5a477b4a70865ef1f2cd1b7594a4c60e188a
Lautaro <lautajamdev@gmail.com>
2023-07-21 20:35:22 -0300

    Deploy version 1
    Settings update v1

--- django_password_generator/settings.py
+++ django_password_generator/settings.py
@@ -104,6 +104,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',          
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
